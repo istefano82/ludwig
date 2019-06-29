77184bd7df0c187ef2ea876ef9e0db24d3beda0a
ivo <ivaylo.stefanov82@yahoo.com>
2019-06-29 19:18:54 +0300

    Create confidence_thresholding_cli wrapper funciton that loads model data for visualisation.

--- tests/integration_tests/test_visualisations.py
+++ tests/integration_tests/test_visualisations.py
@@ -762,11 +762,7 @@ def test_visualisation_cconfidence_thresholding_output_saved(csv_filename):
     vis_patterns = [vis_output_pattern_pdf, vis_output_pattern_png]
 
     for command, viz_pattern in zip(commands, vis_patterns):
-        result = subprocess.run(
-            command,
-            # stdout=subprocess.PIPE,
-            # stderr=subprocess.PIPE
-        )
+        result = subprocess.run(command)
         figure_cnt = glob.glob(viz_pattern)
 
         assert 0 == result.returncode
