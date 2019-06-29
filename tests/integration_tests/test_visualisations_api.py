ba099a06508013a67d5d35eb1caf5017de22631e
ivaylo_stefanov82@yahoo.com <ivaylo_stefanov82@yahoo.com>
2019-06-29 10:39:07 +0300

    Reformat to ensure PEP8 standarts are met.

--- tests/integration_tests/test_visualisations_api.py
+++ tests/integration_tests/test_visualisations_api.py
@@ -14,23 +14,22 @@
 # limitations under the License.
 # ==============================================================================
 
-import shutil
 import glob
-import pandas as pd
+import shutil
+
 import numpy as np
+import pandas as pd
 
 from ludwig import visualize
 from ludwig.api import LudwigModel
-from ludwig.data.preprocessing import _preprocess_csv_for_training, get_split
-from ludwig.utils.data_utils import read_csv, load_json, load_from_file, \
-    split_dataset_tvt
-
+from ludwig.data.preprocessing import get_split
+from ludwig.utils.data_utils import read_csv, split_dataset_tvt
 from tests.integration_tests.utils import categorical_feature, \
     numerical_feature, set_feature, generate_data, sequence_feature, \
     text_feature, binary_feature, bag_feature
+
 # The following imports are pytest fixtures, required for running the tests
 from tests.fixtures.filenames import csv_filename
-
 
 
 def run_api_experiment(input_features, output_features):
@@ -38,7 +37,6 @@ def run_api_experiment(input_features, output_features):
     Helper method to avoid code repetition in running an experiment
     :param input_features: input schema
     :param output_features: output schema
-    :param data_csv: path to data
     :return: None
     """
     model_definition = {
@@ -50,6 +48,7 @@ def run_api_experiment(input_features, output_features):
 
     model = LudwigModel(model_definition)
     return model
+
 
 class Experiment:
     """Helper class to create model test data, setup and run experiment.
@@ -86,8 +85,9 @@ class Experiment:
             data_df=test_df
         )
         self.field = self.output_features[0]['name']
-        # probabilities need to be list of lists containing each row data from the
-        # probability columns ref: https://uber.github.io/ludwig/api/#test - Return
+        # probabilities need to be list of lists containing each row data
+        # from the probability columns
+        # ref: https://uber.github.io/ludwig/api/#test - Return
         self.probability = self.test_stats_full[0].iloc[:, 2:].values
         self.ground_truth_metadata = self.model.train_set_metadata
         target_predictions = test_df[self.field]
@@ -100,7 +100,7 @@ class Experiment:
             self.ground_truth_metadata[self.field]['str2idx'][pred_row]
             for pred_row in self.prediction_raw])
 
-    def run_api_experiment(self,):
+    def run_api_experiment(self, ):
         """Helper method to avoid code repetition in running an experiment"""
         model_definition = {
             'input_features': self.input_features,
@@ -128,6 +128,7 @@ def obtain_df_splits(data_csv):
     train_df = pd.DataFrame(train_split)
     val_df = pd.DataFrame(val_split)
     return test_df, train_df, val_df
+
 
 def test_learning_curves_vis_api(csv_filename):
     """Ensure pdf and png figures can be saved via visualisation API call.
@@ -167,7 +168,7 @@ def test_compare_performance_vis_api(csv_filename):
         visualize.compare_performance(
             [test_stats, test_stats],
             field=None,
-            model_namess = ['Model1', 'Model2'],
+            model_namess=['Model1', 'Model2'],
             output_directory=experiment.model.exp_dir_name,
             file_format=viz_output
         )
@@ -193,13 +194,14 @@ def test_compare_classifier_performance_from_prob_vis_api(csv_filename):
             experiment.ground_truth,
             top_n_classes=[0],
             labels_limit=0,
-            model_namess = ['Model1', 'Model2'],
+            model_namess=['Model1', 'Model2'],
             output_directory=experiment.model.exp_dir_name,
             file_format=viz_output
         )
         figure_cnt = glob.glob(vis_output_pattern_pdf)
         assert 1 == len(figure_cnt)
     shutil.rmtree(experiment.model.exp_dir_name, ignore_errors=True)
+
 
 def test_compare_classifier_performance_from_pred_vis_api(csv_filename):
     """Ensure pdf and png figures can be saved via visualisation API call.
@@ -219,7 +221,7 @@ def test_compare_classifier_performance_from_pred_vis_api(csv_filename):
             experiment.ground_truth_metadata,
             experiment.field,
             labels_limit=0,
-            model_namess = ['Model1', 'Model2'],
+            model_namess=['Model1', 'Model2'],
             output_directory=experiment.model.exp_dir_name,
             file_format=viz_output
         )
@@ -246,13 +248,14 @@ def test_compare_classifiers_performance_subset_vis_api(csv_filename):
             top_n_classes=[6],
             labels_limit=0,
             subset='ground_truth',
-            model_namess = ['Model1', 'Model2'],
+            model_namess=['Model1', 'Model2'],
             output_directory=experiment.model.exp_dir_name,
             file_format=viz_output
         )
         figure_cnt = glob.glob(vis_output_pattern_pdf)
         assert 1 == len(figure_cnt)
     shutil.rmtree(experiment.model.exp_dir_name, ignore_errors=True)
+
 
 def test_compare_classifiers_performance_changing_k_vis_api(csv_filename):
     """Ensure pdf and png figures can be saved via visualisation API call.
@@ -272,7 +275,7 @@ def test_compare_classifiers_performance_changing_k_vis_api(csv_filename):
             experiment.ground_truth,
             top_k=3,
             labels_limit=0,
-            model_namess = ['Model1', 'Model2'],
+            model_namess=['Model1', 'Model2'],
             output_directory=experiment.model.exp_dir_name,
             file_format=viz_output
         )
@@ -299,13 +302,14 @@ def test_compare_classifiers_multiclass_multimetric_vis_api(csv_filename):
             experiment.ground_truth_metadata,
             experiment.field,
             top_n_classes=[6],
-            model_namess = ['Model1', 'Model2'],
+            model_namess=['Model1', 'Model2'],
             output_directory=experiment.model.exp_dir_name,
             file_format=viz_output
         )
         figure_cnt = glob.glob(vis_output_pattern_pdf)
         assert 4 == len(figure_cnt)
     shutil.rmtree(experiment.model.exp_dir_name, ignore_errors=True)
+
 
 def test_compare_classifiers_predictions_vis_api(csv_filename):
     """Ensure pdf and png figures can be saved via visualisation API call.
@@ -324,13 +328,14 @@ def test_compare_classifiers_predictions_vis_api(csv_filename):
             [prediction, prediction],
             experiment.ground_truth,
             labels_limit=0,
-            model_names = ['Model1', 'Model2'],
+            model_names=['Model1', 'Model2'],
             output_directory=experiment.model.exp_dir_name,
             file_format=viz_output
         )
         figure_cnt = glob.glob(vis_output_pattern_pdf)
         assert 1 == len(figure_cnt)
     shutil.rmtree(experiment.model.exp_dir_name, ignore_errors=True)
+
 
 def test_compare_classifiers_predictions_distribution_vis_api(csv_filename):
     """Ensure pdf and png figures can be saved via visualisation API call.
@@ -349,13 +354,14 @@ def test_compare_classifiers_predictions_distribution_vis_api(csv_filename):
             [prediction, prediction],
             experiment.ground_truth,
             labels_limit=0,
-            model_names = ['Model1', 'Model2'],
+            model_names=['Model1', 'Model2'],
             output_directory=experiment.model.exp_dir_name,
             file_format=viz_output
         )
         figure_cnt = glob.glob(vis_output_pattern_pdf)
         assert 1 == len(figure_cnt)
     shutil.rmtree(experiment.model.exp_dir_name, ignore_errors=True)
+
 
 def test_confidence_thresholding_vis_api(csv_filename):
     """Ensure pdf and png figures can be saved via visualisation API call.
@@ -374,7 +380,7 @@ def test_confidence_thresholding_vis_api(csv_filename):
             [probability, probability],
             experiment.ground_truth,
             labels_limit=0,
-            model_names = ['Model1', 'Model2'],
+            model_names=['Model1', 'Model2'],
             output_directory=experiment.model.exp_dir_name,
             file_format=viz_output
         )
@@ -382,7 +388,8 @@ def test_confidence_thresholding_vis_api(csv_filename):
         assert 1 == len(figure_cnt)
     shutil.rmtree(experiment.model.exp_dir_name, ignore_errors=True)
 
-def test_confidence_thresholding_vis_api(csv_filename):
+
+def test_confidence_thresholding_data_vs_acc_vis_api(csv_filename):
     """Ensure pdf and png figures can be saved via visualisation API call.
 
     :param csv_filename: csv fixture from tests.fixtures.filenames.csv_filename
@@ -399,13 +406,14 @@ def test_confidence_thresholding_vis_api(csv_filename):
             [probability, probability],
             experiment.ground_truth,
             labels_limit=0,
-            model_names = ['Model1', 'Model2'],
+            model_names=['Model1', 'Model2'],
             output_directory=experiment.model.exp_dir_name,
             file_format=viz_output
         )
         figure_cnt = glob.glob(vis_output_pattern_pdf)
         assert 1 == len(figure_cnt)
     shutil.rmtree(experiment.model.exp_dir_name, ignore_errors=True)
+
 
 def test_confidence_thresholding_data_vs_acc_subset_vis_api(csv_filename):
     """Ensure pdf and png figures can be saved via visualisation API call.
@@ -426,13 +434,14 @@ def test_confidence_thresholding_data_vs_acc_subset_vis_api(csv_filename):
             top_n_classes=[3],
             labels_limit=0,
             subset='ground_truth',
-            model_names = ['Model1', 'Model2'],
+            model_names=['Model1', 'Model2'],
             output_directory=experiment.model.exp_dir_name,
             file_format=viz_output
         )
         figure_cnt = glob.glob(vis_output_pattern_pdf)
         assert 1 == len(figure_cnt)
     shutil.rmtree(experiment.model.exp_dir_name, ignore_errors=True)
+
 
 def test_confidence_thresholding_data_vs_acc_subset_per_class_vis_api(
         csv_filename
@@ -457,7 +466,7 @@ def test_confidence_thresholding_data_vs_acc_subset_per_class_vis_api(
             top_n_classes=[3],
             labels_limit=0,
             subset='ground_truth',
-            model_names = ['Model1', 'Model2'],
+            model_names=['Model1', 'Model2'],
             output_directory=experiment.model.exp_dir_name,
             file_format=viz_output
         )
@@ -466,6 +475,7 @@ def test_confidence_thresholding_data_vs_acc_subset_per_class_vis_api(
         # hence one figure per class
         assert 3 == len(figure_cnt)
     shutil.rmtree(experiment.model.exp_dir_name, ignore_errors=True)
+
 
 def test_confidence_thresholding_2thresholds_2d_vis_api(csv_filename):
     """Ensure pdf and png figures can be saved via visualisation API call.
@@ -502,8 +512,8 @@ def test_confidence_thresholding_2thresholds_2d_vis_api(csv_filename):
     field2 = output_features[1]['name']
     # probabilities need to be list of lists containing each row data from the
     # probability columns ref: https://uber.github.io/ludwig/api/#test - Return
-    probability1 = test_stats[0].iloc[:, [2,3,4]].values
-    probability2 = test_stats[0].iloc[:, [7,8,9]].values
+    probability1 = test_stats[0].iloc[:, [2, 3, 4]].values
+    probability2 = test_stats[0].iloc[:, [7, 8, 9]].values
 
     ground_truth_metadata = model.train_set_metadata
     target_predictions1 = test_df[field1]
@@ -524,13 +534,14 @@ def test_confidence_thresholding_2thresholds_2d_vis_api(csv_filename):
             [ground_truth1, ground_truth2],
             [field1, field2],
             labels_limit=0,
-            model_names = ['Model1'],
+            model_names=['Model1'],
             output_directory=model.exp_dir_name,
             file_format=viz_output
         )
         figure_cnt = glob.glob(vis_output_pattern_pdf)
         assert 3 == len(figure_cnt)
     shutil.rmtree(model.exp_dir_name, ignore_errors=True)
+
 
 def test_confidence_thresholding_2thresholds_3d_vis_api(csv_filename):
     """Ensure pdf and png figures can be saved via visualisation API call.
@@ -567,8 +578,8 @@ def test_confidence_thresholding_2thresholds_3d_vis_api(csv_filename):
     field2 = output_features[1]['name']
     # probabilities need to be list of lists containing each row data from the
     # probability columns ref: https://uber.github.io/ludwig/api/#test - Return
-    probability1 = test_stats[0].iloc[:, [2,3,4]].values
-    probability2 = test_stats[0].iloc[:, [7,8,9]].values
+    probability1 = test_stats[0].iloc[:, [2, 3, 4]].values
+    probability2 = test_stats[0].iloc[:, [7, 8, 9]].values
 
     ground_truth_metadata = model.train_set_metadata
     target_predictions1 = test_df[field1]
@@ -596,6 +607,7 @@ def test_confidence_thresholding_2thresholds_3d_vis_api(csv_filename):
         assert 1 == len(figure_cnt)
     shutil.rmtree(model.exp_dir_name, ignore_errors=True)
 
+
 def test_binary_threshold_vs_metric_vis_api(csv_filename):
     """Ensure pdf and png figures can be saved via visualisation API call.
 
@@ -616,13 +628,14 @@ def test_binary_threshold_vs_metric_vis_api(csv_filename):
             experiment.ground_truth,
             metrics,
             positive_label,
-            model_names = ['Model1', 'Model2'],
+            model_names=['Model1', 'Model2'],
             output_directory=experiment.model.exp_dir_name,
             file_format=viz_output
         )
         figure_cnt = glob.glob(vis_output_pattern_pdf)
         assert 1 == len(figure_cnt)
     shutil.rmtree(experiment.model.exp_dir_name, ignore_errors=True)
+
 
 def test_roc_curves_vis_api(csv_filename):
     """Ensure pdf and png figures can be saved via visualisation API call.
@@ -642,13 +655,14 @@ def test_roc_curves_vis_api(csv_filename):
             [probability, probability],
             experiment.ground_truth,
             positive_label,
-            model_names = ['Model1', 'Model2'],
+            model_names=['Model1', 'Model2'],
             output_directory=experiment.model.exp_dir_name,
             file_format=viz_output
         )
         figure_cnt = glob.glob(vis_output_pattern_pdf)
         assert 1 == len(figure_cnt)
     shutil.rmtree(experiment.model.exp_dir_name, ignore_errors=True)
+
 
 def test_roc_curves_from_test_statistics_vis_api(csv_filename):
     """Ensure pdf and png figures can be saved via visualisation API call.
@@ -674,13 +688,14 @@ def test_roc_curves_from_test_statistics_vis_api(csv_filename):
         visualize.roc_curves_from_test_statistics(
             [test_stats, test_stats],
             field,
-            model_namess = ['Model1', 'Model2'],
+            model_namess=['Model1', 'Model2'],
             output_directory=model.exp_dir_name,
             file_format=viz_output
         )
         figure_cnt = glob.glob(vis_output_pattern_pdf)
         assert 1 == len(figure_cnt)
     shutil.rmtree(model.exp_dir_name, ignore_errors=True)
+
 
 def test_calibration_1_vs_all_vis_api(csv_filename):
     """Ensure pdf and png figures can be saved via visualisation API call.
@@ -700,13 +715,14 @@ def test_calibration_1_vs_all_vis_api(csv_filename):
             experiment.ground_truth,
             top_n_classes=[6],
             labels_limit=0,
-            model_namess = ['Model1', 'Model2'],
+            model_namess=['Model1', 'Model2'],
             output_directory=experiment.model.exp_dir_name,
             file_format=viz_output
         )
         figure_cnt = glob.glob(vis_output_pattern_pdf)
         assert 5 == len(figure_cnt)
     shutil.rmtree(experiment.model.exp_dir_name, ignore_errors=True)
+
 
 def test_calibration_multiclass_vis_api(csv_filename):
     """Ensure pdf and png figures can be saved via visualisation API call.
@@ -725,13 +741,14 @@ def test_calibration_multiclass_vis_api(csv_filename):
             [probability, probability],
             experiment.ground_truth,
             labels_limit=0,
-            model_names = ['Model1', 'Model2'],
+            model_names=['Model1', 'Model2'],
             output_directory=experiment.model.exp_dir_name,
             file_format=viz_output
         )
         figure_cnt = glob.glob(vis_output_pattern_pdf)
         assert 2 == len(figure_cnt)
     shutil.rmtree(experiment.model.exp_dir_name, ignore_errors=True)
+
 
 def test_confusion_matrix_vis_api(csv_filename):
     """Ensure pdf and png figures can be saved via visualisation API call.
@@ -752,7 +769,7 @@ def test_confusion_matrix_vis_api(csv_filename):
             experiment.field,
             top_n_classes=[0],
             normalize=False,
-            model_names= ['Model1', 'Model2'],
+            model_names=['Model1', 'Model2'],
             output_directory=experiment.model.exp_dir_name,
             file_format=viz_output
         )
@@ -779,11 +796,10 @@ def test_frequency_vs_f1_vis_api(csv_filename):
             experiment.ground_truth_metadata,
             experiment.field,
             top_n_classes=[0],
-            model_names= ['Model1', 'Model2'],
+            model_names=['Model1', 'Model2'],
             output_directory=experiment.model.exp_dir_name,
             file_format=viz_output
         )
         figure_cnt = glob.glob(vis_output_pattern_pdf)
         assert 2 == len(figure_cnt)
     shutil.rmtree(experiment.model.exp_dir_name, ignore_errors=True)
-
